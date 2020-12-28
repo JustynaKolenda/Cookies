df760038fe5e24e1561206f47d5c729b4a5e79b5
Justyna Kolenda <justynakolenda@MacBook-Pro-Justyna.local>
2020-12-28 17:44:57 +0100

    add cookies

---
+++ src/navigation/BotomNav.tsx
@@ -0,0 +1,36 @@
+// // You can import Ionicons from @expo/vector-icons/Ionicons if you use Expo or
+// // react-native-vector-icons/Ionicons otherwise.
+
+// const Tab = cre
+
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Tab.Navigator
+//         screenOptions={({ route }) => ({
+//           tabBarIcon: ({ focused, color, size }) => {
+//             let iconName;
+
+//             if (route.name === 'Home') {
+//               iconName = focused
+//                 ? 'ios-information-circle'
+//                 : 'ios-information-circle-outline';
+//             } else if (route.name === 'Settings') {
+//               iconName = focused ? 'ios-list-box' : 'ios-list';
+//             }
+
+//             // You can return any component that you like here!
+//             return <Ionicons name={iconName} size={size} color={color} />;
+//           },
+//         })}
+//         tabBarOptions={{
+//           activeTintColor: 'tomato',
+//           inactiveTintColor: 'gray',
+//         }}
+//       >
+//         <Tab.Screen name="Home" component={HomeScreen} />
+//       </Tab.Navigator>
+//     </NavigationContainer>
+//   );
+// }
--- src/view/CookieClik.tsx
+++ src/view/CookieClik.tsx
@@ -1,19 +1,63 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { Dimensions, Text, View, Animated, Easing} from 'react-native';
 import styled from 'styled-components/native';
 
-
+const {width, height} = Dimensions.get('window')
 
 const CookieClik = () => {
+    const [count, setCount] = useState(0);
+    const handleIncrement = () => {
+        setCount(prevCount => prevCount + 1);
+    };
 
+
+    
     return(
-        <Cookie source={{uri: 'bigcookie'}}/> 
+        <ViewCookie>
+            <Image source={{uri:'stars'}}>
+                <State>{count} cookies</State>
+                <Click onPress={handleIncrement}>
+                    <Cookie source={{uri: 'bigcookie'}}/> 
+                </Click>
+            </Image>
+        </ViewCookie>
     )
 }
 
-const Cookie = styled.Image({
-    width: 200,
-    height: 200,
+const ViewCookie = styled.View({
+    backgroundColor: '#000000',
+    justifyContent: 'center',
+    alignItems: 'center',
+})
+const Image = styled.ImageBackground({
+    resizeMode: "cover",
+    width: '100%',
+    height:'100%',
+    alignItems:'center',
+    justifyContent:'center'
+})
+const State = styled.Text({
+    color: '#000000',
+    fontSize: 28,
+    fontWeight: 'bold',
+    position: 'absolute',
+    top: 40
+})
+
+const Click = styled.TouchableOpacity({
+    alignItems:'center'
+})
+
+let scaleValue = new Animated.Value(0);
+const cookieScale = scaleValue.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: [1, 1.1, 1.2]
+});
+
+const Cookie = styled(Animated.Image)({
+    width: width/2,
+    height: height/4,
+    marginBottom: 20,
 })
 
 export default CookieClik
